bc6a78068d532de0b6b4dc7881fcda7682c4052a
shinchan12345678 <uv4171@gmail.com>
2022-06-05 02:27:52 +0000

    [Add]開発環境の追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "68287874ec8143dab1009433c67ace83.vfs.cloud9.us-east-1.amazonaws.com"
 end
