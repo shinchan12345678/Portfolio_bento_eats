8f6439e6172e24cc5bc0e45301867c3c2e748266
shinchan12345678 <uv4171@gmail.com>
2022-06-13 01:12:09 +0000

    [Update]GoogleMap機能

--- app/controllers/owner/informations_controller.rb
+++ app/controllers/owner/informations_controller.rb
@@ -1,13 +1,8 @@
 class Owner::InformationsController < ApplicationController
   def new
+    gon.latitude = 35.4577205 # 岐阜の緯度
+    gon.longitude = 136.793811 # 岐阜の経度
     @information = Information.new
-    default_sw_t = [35.443736713821224, 136.76085201562503]
-    default_ne_t = [35.47170185598229, 136.82676998437503]
-    latitude_s, longitude_s = default_sw_t.map { |i| i }
-    latitude_e, longitude_e = default_ne_t.map { |i| i }
-    @informations = Information.map_view_ports(latitude_s, latitude_e, longitude_s, longitude_e)
-    gon.informations = @informations
-    # binding.pry
   end
 
   def create
--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,131 +1,142 @@
-var lat = gon.latitude;
-var lng = gon.longitude;
-var marker = null;
-var latlng = null;
-const uluru = { lat: lat, lng: lng };
 // ---------------------------------------------------------------------------------------------------
 // 会員の投稿一覧画面表示
 function initMapCustomer() {
-  // 地図の表示
-  const map_customer = document.getElementById("map_customer");
-  var map = mapping(map_customer);
-  var latlng = new google.maps.LatLng(lat, lng);
-  marker = new google.maps.Marker({
-		position: latlng, //マーカーの位置
-		map: map //マーカーを表示する地図
-	});
+  initilize.then(function(){
+    console.log("成功2");
+    // 地図の表示
+    const map_customer = document.getElementById("map_customer");
+    var map = mapping(map_customer);
+    var latlng = new google.maps.LatLng(lat, lng);
+    marker = new google.maps.Marker({
+		  position: latlng, //マーカーの位置
+		  map: map //マーカーを表示する地図
+	  });
 
-  google.maps.event.addListener(map, 'bounds_changed', function() {
-    mapBounds();
-    latlngInsert();
-    // 現在地を取得するイベントを設定
-    (function(){
-      var element = document.getElementById("customer_now_postion_get");
-      positionGet(element);
-      element.addEventListener("click",function(){
-        mapBounds();
-        latlngInsert();
-      });
-    })();
+    google.maps.event.addListener(map, 'bounds_changed', function() {
+      mapBounds();
+      latlngInsert();
+      // 現在地を取得するイベントを設定
+      (function(){
+        var element = document.getElementById("customer_now_postion_get");
+        positionGet(element);
+        element.addEventListener("click",function(){
+          mapBounds();
+          latlngInsert();
+        });
+      })();
 
-    // 住所検索し位置取得するイベントを設定
-    (function(){
-      var element = document.getElementById('address_form_btn');
-      positionSearch(element);
-      element.addEventListener("click",function(){
-        mapBounds();
-        latlngInsert();
-      });
-    })();
+      // 住所検索し位置取得するイベントを設定
+      (function(){
+        var element = document.getElementById('address_form_btn');
+        positionSearch(element);
+        element.addEventListener("click",function(){
+          mapBounds();
+          latlngInsert();
+        });
+      })();
 
-    // クリックした位置にマーカーを移動するイベントを設定
-    (function () {
-      map.addListener("click", function(event) {
-        updateMarker(event.latLng);
-        mapBounds();
-        latlngInsert();
-      })
-    })();
-  });
+      // クリックした位置にマーカーを移動するイベントを設定
+      (function () {
+        map.addListener("click", function(event) {
+          updateMarker(event.latLng);
+          mapBounds();
+          latlngInsert();
+        })
+      })();
 
-  // 投稿情報を取得して地図上に表示
-  if (gon.informations) {
-    var infowindow = new google.maps.InfoWindow({});
-    for (var i = 0; i < gon.informations.length; i++ ) {
-      var fragLatLng = new google.maps.LatLng({
-        lat: parseFloat(gon.informations[i]['latitude']),
-        lng: parseFloat(gon.informations[i]['longitude'])
-      });
+      // 投稿情報を取得して地図上に表示
+      if (gon.informations) {
+        var infowindow = new google.maps.InfoWindow({});
+        for (var i = 0; i < gon.informations.length; i++ ) {
+          var fragLatLng = new google.maps.LatLng({
+            lat: parseFloat(gon.informations[i]['latitude']),
+            lng: parseFloat(gon.informations[i]['longitude'])
+          });
 
-      var frag = new google.maps.Marker({
-        position: fragLatLng,
-        map: map,
-        icon: {
-          url: "/assets/frag.png",
-          scaledSize: new google.maps.Size( 40, 97 )
+          var frag = new google.maps.Marker({
+            position: fragLatLng,
+            map: map,
+            icon: {
+              url: "/assets/frag.png",
+              scaledSize: new google.maps.Size( 40, 97 )
+            }
+          });
+
+          attachMessage(frag, gon.informations[i]['longitude']);
         }
-      });
-
-      attachMessage(frag, gon.informations[i]['longitude']);
-    }
-  }
+      }
+    });
+  })
 }
 
 // ---------------------------------------------------------------------------------------------------
 // 飲食店オーナーの新規投稿画面表示
 function initMapOwner() {
-  // 地図の表示
-  const map_owner = document.getElementById("map_owner")
-  var map = mapping(map_owner);
-  var latlng = new google.maps.LatLng(lat, lng);
-  marker = new google.maps.Marker({
-		position: latlng,
-		map: map
-	});
+  initilize.then(function(){
+    // 地図の表示
+    const map_owner = document.getElementById("map_owner")
+    var map = mapping(map_owner);
+    var latlng = new google.maps.LatLng(lat, lng);
+    marker = new google.maps.Marker({
+  		position: latlng,
+  		map: map
+  	});
 
-	google.maps.event.addListener(map, 'bounds_changed', function() {
-	  latlngInsert();
+  	google.maps.event.addListener(map, 'bounds_changed', function() {
+  	  latlngInsert();
 
-    // 現在地を取得するイベントを設定
+      // 現在地を取得するイベントを設定
+      (function(){
+        var element = document.getElementById("owner_now_position_get");
+        positionGet(element);
+        element.addEventListener("click",function(){
+          console.log(element);
+          latlngInsert();
+        });
+      })();
+
+      // 住所検索し位置取得するイベントを設定
+      (function(){
+        var element = document.getElementById('address_form_btn');
+        positionSearch(element);
+        element.addEventListener("click",function(){
+          latlngInsert();
+        });
+      })();
+
+      // クリックした位置にマーカーを移動するイベントを設定
+      (function () {
+        map.addListener("click", function(event) {
+          updateMarker(event.latLng);
+          latlngInsert();
+        })
+      })();
+  	});
+
+  	 // 投稿情報の設定
     (function(){
-      var element = document.getElementById("owner_now_position_get");
-      positionGet(element);
+      var element = document.getElementById('post_position_get');
       element.addEventListener("click",function(){
         console.log(element);
-        latlngInsert();
+        document.getElementById("information_latitude").value = document.getElementById("information_hidden_lat").value;
+        document.getElementById("information_longitude").value = document.getElementById("information_hidden_lng").value;
       });
     })();
-
-    // 住所検索し位置取得するイベントを設定
-    (function(){
-      var element = document.getElementById('address_form_btn');
-      positionSearch(element);
-      element.addEventListener("click",function(){
-        latlngInsert();
-      });
-    })();
-
-    // クリックした位置にマーカーを移動するイベントを設定
-    (function () {
-      map.addListener("click", function(event) {
-        updateMarker(event.latLng);
-        latlngInsert();
-      })
-    })();
-	});
-
-	 // 投稿情報の設定
-  (function(){
-    var element = document.getElementById('post_position_get');
-    element.addEventListener("click",function(){
-      console.log(element);
-      document.getElementById("information_latitude").value = document.getElementById("information_hidden_lat").value;
-      document.getElementById("information_longitude").value = document.getElementById("information_hidden_lng").value;
-    });
-  })();
+  })
 }
 
 // ---------------------------------------------------------------------------------------------------
+// 座標データを初期化する関数
+var initilize = new Promise(function(resolve) {
+  lat = gon.latitude ? gon.latitude:null;
+  lng = gon.longitude ? gon.longitude:null;
+  var marker = null;
+  var latlng = null;
+  uluru = { lat: lat, lng: lng };
+  console.log("成功");
+  resolve();
+});
+
 // Mapオブジェクトを生成する関数
 function mapping(map_view) {
   map = new google.maps.Map(map_view, { // map_viewには地図を表示領域要素を設定
@@ -205,5 +216,6 @@ function attachMessage(marker,msg) {
     }).open(marker.getMap(),marker);
   });
 }
+
 window.initMapOwner = initMapOwner;
 window.initMapCustomer = initMapCustomer;
