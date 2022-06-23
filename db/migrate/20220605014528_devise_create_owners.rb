941223631fa3f0d30311d19d33c843b9b663e6f5
shinchan12345678 <uv4171@gmail.com>
2022-06-23 02:24:18 +0000

    [Update]オーナーのマイグレーション設定変更

--- db/migrate/20220605014528_devise_create_owners.rb
+++ db/migrate/20220605014528_devise_create_owners.rb
@@ -33,7 +33,7 @@ class DeviseCreateOwners < ActiveRecord::Migration[6.1]
       # t.datetime :locked_at
 
       t.string :name, null: false
-      t.string :address ,index: true, null: false
+      t.string :address , null: false
       t.string :telphone_number, null: false
       t.text :introduction
 
--- db/schema.rb
+++ db/schema.rb
@@ -116,7 +116,6 @@ ActiveRecord::Schema.define(version: 2022_06_16_014618) do
     t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["address"], name: "index_owners_on_address"
     t.index ["email"], name: "index_owners_on_email", unique: true
     t.index ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true
   end
