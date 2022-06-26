a357e01013ac83e613f382fea8e5ef7355eb2120
shinchan12345678 <uv4171@gmail.com>
2022-06-26 07:59:50 +0000

    [Add]バッチ処理

---
+++ config/schedule.rb
@@ -0,0 +1,35 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+
+require File.expand_path(File.dirname(__FILE__) + "/environment") # Rails.rootを使用するために必要
+rails_env = Rails.env.to_sym # cronを実行する環境変数
+set :environment, rails_env # cronを実行する環境変数をセット
+set :output, 'log/cron.log' # cronのログの吐き出し場所
+every 1.day, at: '0:00 am' do # 毎日午前0時に実行
+# rubocop:disable all
+  begin
+    runner "Batch::InformationUpdate.informationUpdate"
+  rescue => e
+    Rails.logger.error("aborted rails runner")
+    raise e
+  end
+# rubocop:enable all
+end
