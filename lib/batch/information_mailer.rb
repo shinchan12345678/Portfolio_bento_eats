692a858a367712fabb92abbdf35eb176e78ad6ef
shinchan12345678 <uv4171@gmail.com>
2022-07-16 07:51:00 +0000

    [Fix]メール通知用バッジファイル

--- lib/batch/information_mailer.rb
+++ lib/batch/information_mailer.rb
@@ -1,6 +1,6 @@
 class Batch::InformationMailer
   def self.informationMailer
-    informations_is_today = Information.where("open_date == ? and is_valid = ?", Date.current, 0)
+    informations_is_today = Information.where("open_date = ? and is_valid = ?", Date.current, 0)
     informations_is_today.each do |info|
       OwnerMailer.creation_email(info).deliver_now
     end
